4397b421ad999ff67330cd4cf213ebfe0e87d7e5
khamdaeng <118417111+khamdaengpimma@users.noreply.github.com>
2023-11-26 13:15:26 +0700

    Add files via upload

--- password.js
+++ password.js
@@ -1,11 +1,12 @@
-const pw = ["127.0.0.1", "ABDF"];
+const pw = {"127.0.0.1": "Level_1", "ABDF": "Level_2", "subscribetokhamdaeng": "Level_3"};
 
 exports.password = (req, res) => {
   const pass = req.params.password;
 
-  if (pw.includes(pass)) {
-    res.send("khamdaeng");
+  if (pw.hasOwnProperty(pass)) {
+    const level = pw[pass];
+    res.send(`${level}`);
   } else {
-    res.send("wrong password");
+    res.send("NO!");
   }
 };
